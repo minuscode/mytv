8616c3ad03757221a408c836ea6062813c981ab5
Miguel Marinhas <miguel.marinhas@gmail.com>
2013-01-08 22:44:29 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytv::Application.config.secret_token = '45a3bf54d88920afbe19847e77f33a5ef6afe7da5410d7f311933ab57aa99aa57923a6c3afb262543bad49a228120b2e966f831f83404b06d89e84fda06368ac'
